5d62c13759768f8ab4f8daae05bf9188216dbdd5
Chris Jackson <sccjj@leeds.ac.uk>
2024-03-04 15:52:42 +0000

    add doc string

--- hype/hype.py
+++ hype/hype.py
@@ -1,7 +1,17 @@
 def square(a):
+    '''this is square function'''
     return a*a
 
 def sum(a,b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
     return a+b
 
 def squareroot(b):
