4f565026a7f3e524918a50896b54aa2c53bde99f
Luke Barratt <scljb@leeds.ac.uk>
2024-03-04 15:08:52 +0000

    fix sum

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -6,11 +6,11 @@ def test_square():
     assert hh.square(-2) == 4, 'neg should result pos'
 
 def test_sum():
-    assert hh.square(4, 4) == 8, 'sum no work'
+    assert hh.sum(4, 4) == 8, 'sum no work'
 
 def test_root():
     assert hh.squareroot(4) == 2, 'root no work'
     assert hh.squareroot(-4) == 'UNDEFINED', 'negative root no define'
 
 def test_hype():
-    assert hh.hypotenuse(3, 4) == 5, 'hype no work'
+    assert hh.hypotenuse(3, 4) == 5, 'hype no work' 
